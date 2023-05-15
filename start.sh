4cd790b97e2f7159553a13626c112eb46af1270b
mjhytgvfdsxzl2022 <132864085+mjhytgvfdsxzl2022@users.noreply.github.com>
2023-05-15 07:45:56 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 #请修改为自己的UUID
-export UUID=ba3f6e6d-0648-4b39-b35d-36d2355e719
+export UUID=f5ff309c-7e5f-4d7b-90fb-32dca2bf1039
 
 #请修改为自己设置的伪装站，不要带https://
-export ProxySite=www.holehike.com
+export ProxySite=www.booking.com
 
 #端口，如无特殊需求请勿更改,如果要改请一并修改dockerfile中的端口
 export Port=8080
